96361f9ca5eefb1b19d84e4d21ba0a993f7e877a
Vladimir Blake <mcpebyjohan@gmail.com>
2023-05-06 16:10:11 +0300

    add react-router and main routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import './assets/css/App.css';
+import { Home } from './pages/Home';
+import { Settings } from './pages/Settings';
+import { Identities } from './pages/Identities';
+import { CustomNode } from './pages/CustomNode';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main className='main'>
+      <BrowserRouter>
+          <Routes>
+            <Route path='' element={<Home/>}/>
+            <Route path='settings' element={<Settings/>}/>
+            <Route path='identities' element={<Identities/>}/>
+            <Route path='custom-node' element={<CustomNode/>}/>
+          </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
